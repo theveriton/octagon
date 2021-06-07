016034b7eca6cccbd9fcaab7eb7e32e64af17e23
Aryamann Singh <singhrobin1122334455@gmail.com>
2021-06-07 16:40:19 +0530

    english word added and home made

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @Override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-    )
+        theme: ThemeData(primaryColor: Colors.purple[700]),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Word Pair Generator'),
+              centerTitle: true,
+            ),
+            body: Center(
+              child: Text('This is the body'),
+            )));
   }
 }
